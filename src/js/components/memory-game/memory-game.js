d9508466f479b439d3cf1176358639dce5770041
Hannah Obote <ao223ir@student.lnu.se>
2021-11-07 19:04:57 +0100

    add front card for each card.

--- src/js/components/memory-game/memory-game.js
+++ src/js/components/memory-game/memory-game.js
@@ -6,8 +6,7 @@ import '../my-card/my-card.js'
  */
  const template = document.createElement('template') 
  template.innerHTML = `
- 
- 
+
  `
 
  /**
@@ -21,19 +20,49 @@ import '../my-card/my-card.js'
     // append the template to the shadow root.
     this.attachShadow({ mode: 'open' })
       .appendChild(template.content.cloneNode(true)) // viktigt !!!!.
+    //this.addCard()
+    this.fourByFour()
+    
+    this.allCardEl = this.shadowRoot.querySelectorAll('my-card')
+    console.log(this.allCardEl)
 
-    this.addAllCards()
     // Get elemens here 
     // TODO: Maybee you need to define some default values here
   }
 
-  
-
-  addAllCards () {
-    cardsdb.forEach(img => {
+  fourByFour() {
+    for(let i=0; i < 12; i++) {
       let card = document.createElement('my-card')
-      card.setAttribute('cardFront', img.cardFront)
-      card.set
+      this.shadowRoot.appendChild( card )
+      cardsdb.forEach(dbItem => {
+        card.setFrontImg(dbItem.cardFront)
+      })
+    }
+  }
+
+  fourByTwo () {
+    for(let i=0; i < 8; i++) {
+      let card = document.createElement('my-card')
+      this.shadowRoot.appendChild( card )
+    }
+  }
+
+  twoByTwo () {
+    for(let i=0; i < 4; i++) {
+      let card = document.createElement('my-card')
+      this.shadowRoot.appendChild( card )
+    }
+  }
+
+
+  addCard() {
+    cardsdb.forEach(db => {
+      let card = document.createElement('my-card')
+      card.src = card.getAttribute('cardFront')
+      let img = document.createElement('img')
+      img.src = db.cardFront
+      img.id = db.id
+      card.shadowRoot.appendChild(img)
       this.shadowRoot.appendChild( card )
     })
   }
--- src/js/components/my-card/my-card.js
+++ src/js/components/my-card/my-card.js
@@ -1,8 +1,8 @@
 /**
  * Define template.
  */
- const template = document.createElement('template') 
- template.innerHTML = `
+const template = document.createElement('template')
+template.innerHTML = `
 
  <style>
  body {
@@ -30,27 +30,19 @@
  }
 
  </style>
-
-<h1> my card  </h1>
- <!--<div id="card">
-  <img id="cardBack">
-  <img id="cardFront">
- </div> -->
-
  <div id="card">
     <img 
       id="cardBack"
       src="https://thumbs.dreamstime.com/b/playing-card-back-side-isolated-white-clipping-path-included-playing-card-back-side-isolated-white-172500899.jpg"
     >
   <img id="cardFront">
- </div>
-
+ </div> 
  `
 
- /**
-  * Runs the app.
-  */
- class myCard extends HTMLElement {
+/**
+ * Runs the app.
+ */
+class myCard extends HTMLElement {
   constructor() {
     super()
 
@@ -58,20 +50,34 @@
     // append the template to the shadow root.
     this.attachShadow({ mode: 'open' })
       .appendChild(template.content.cloneNode(true))
-    this.shadowRoot.querySelector('#cardFront').src = this.getAttribute('cardFront')
+    //this.shadowRoot.querySelector('#cardFront').src = this.getAttribute('cardfront')
     this.cardFront = this.shadowRoot.querySelector('#cardFront')
     this.cardBack = this.shadowRoot.querySelector('#cardBack')
-    
+
     this.showFront = false;
     // Get elemens here 
     // TODO: Maybee you need to define some default values here
   }
 
+  /**
+ * Called when observed attribute(s) changes.
+ *
+ * @param {string} name - The attribute's name.
+ * @param {*} oldValue - The old value.
+ * @param {*} newValue - The new value.
+ */
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === 'boardsize') {
+      return false
+    }
+  }
+
+
   flipCard() {
     // turn card 
     // hide back
     this.showFront = !this.showFront
-    if(this.showFront) {
+    if (this.showFront) {
       this.cardFront.style.visibility = 'visible'
       this.cardBack.style.visibility = 'hidden'
     } else {
@@ -80,14 +86,20 @@
     }
   }
 
+  setFrontImg(src) {
+    //this.cardFront.src = src
+    this.shadowRoot.querySelector('#cardFront').src = src
+    console.log(src)
+  }
+
   connectedCallback() {
     this.cardBack.addEventListener('click', () => this.flipCard())
     this.cardFront.addEventListener('click', () => this.flipCard())
   }
-  
+
   disconnectedCallback() {
 
   }
- }
+}
 
- window.customElements.define('my-card', myCard);
+window.customElements.define('my-card', myCard);
