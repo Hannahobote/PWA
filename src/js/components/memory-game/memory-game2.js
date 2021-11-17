8d45edbed09db07b07c30580c24fc7ad2e5806b6
Hannah Obote <ao223ir@student.lnu.se>
2021-11-17 16:52:48 +0100

    set up board

--- src/js/components/memory-game/memory-game2.js
+++ src/js/components/memory-game/memory-game2.js
@@ -30,27 +30,44 @@ class memoryGame extends HTMLElement {
     this.attachShadow({ mode: 'open' })
       .appendChild(template.content.cloneNode(true)) 
 
-      this.colorData = ['red', 'red', 'blue', 'blue', 'pink', 'pink', 'purple', 'purple', 'green', 'green', 'yellow', 'yellow']
-      this.createCards()
-      console.log(this.shuffleColorData())
+      this.colorData = ['red', 'red', 'blue', 'blue', 'pink', 'pink', 'purple', 'purple', 'green', 'green', 'yellow', 'yellow'] // change to pastel colors :)
+      //this.createCards()
+      this.createFourByFourCards()
+      this.createFourByTwoCards()
+      this.createTwoByTwoCards()
 
   }
   
-  shuffleColorData () {
-    return this.colorData.sort((a, b) => 0.5 - Math.random());
+  shuffleColorData (colorData) {
+    return colorData.sort((a, b) => 0.5 - Math.random());
   }
 
-  createCards () {
-    let data =  this.shuffleColorData()
+  createCards (data) {
     data.forEach(data => {
       let card = document.createElement('my-cardtwo')
       card.dataset.color = data
       card.setColor()
-      console.log(card.dataset.color, 'dataset') 
+      // console.log(card.dataset.color, 'dataset') //  for error handeling.
       this.shadowRoot.querySelector('.board').appendChild(card)
     })
   }
 
+  createFourByFourCards () {
+    let cards = this.shuffleColorData(this.colorData)
+    this.createCards(cards)
+  }
+
+  createFourByTwoCards () {
+    let cards = this.colorData.slice(0,8)
+    cards = this.shuffleColorData(cards)
+    this.createCards(cards)
+  }
+
+  createTwoByTwoCards () {
+    let cards = this.colorData.slice(0,4)
+    cards = this.shuffleColorData(cards)
+    this.createCards(cards)
+  }
 
 
   connectedCallback() {
